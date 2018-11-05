65d7a61fff2bc5a88689d3e8ea7f61d6a0b66e4f
marrstt <letspanic.dude@gmail.com>
2018-11-06 00:23:17 +0300

    added mock test

--- mock.spec.js
+++ mock.spec.js
@@ -5,3 +5,13 @@ test("mock implementation", function () {
   expect(mock("foo")).toBe("bar");
   expect(mock).toHaveBeenCalledWith("foo");
 });
+
+test("mock implementation one time", () => {
+  const mock = jest.fn().mockImplementationOnce(() => "bar");
+
+  expect(mock("foo")).toBe("bar");
+  expect(mock).toHaveBeenCalledWith("foo");
+
+  expect(mock("baz")).toBe(undefined);
+  expect(mock).toHaveBeenCalledWith("baz");
+});
